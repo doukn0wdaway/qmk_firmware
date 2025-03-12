20788072c5199bf961fdbe46a7a3aee60a590752
doukn0wdaway <anisimov.sasha20@gmail.com>
2025-03-12 02:13:03 +0200

    bootmagic added

--- keyboards/etraxis/encorne/rules.mk
+++ keyboards/etraxis/encorne/rules.mk
@@ -1,6 +1,7 @@
 # This file intentionally left blank
 SPLIT_KEYBOARD = yes
+BOOTMAGIC_ENABLE = yes
 COMBO_ENABLE = yes
-# ENCODER_ENABLE = yes
-# ENCODER_MAP_ENABLE = yes
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
 LEADER_ENABLE = yes
